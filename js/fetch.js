8d5477bfcd71f5d69b6f9ef7bc6ce6e859c51aca
itsmebroarif <aripstrike@gmail.com>
2022-06-09 13:47:10 +0700

    Versi Pertama

---
+++ js/fetch.js
@@ -0,0 +1,28 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxiDRozzRxwdXM21IctWpX0wcC9XeXIzhVHSQEnm2s1ElLwlxZhkccGf5LKNhECA01j_Q/exec";
+const form = document.forms["feedback-form"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // ketika tombol submit diklik
+  // tampilkan tombol loading, hilangkan tombol kirim
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      // tampilkan tombol kirim, hilangkan tombol loading
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      // tampilkan alert
+      myAlert.classList.toggle("d-none");
+      // reset formnya
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
